50f38fe4df84a7c9a71c0ec40ac66be51aa46962
PraveenVishvakarma <praveen15100@gmail.com>
2023-05-21 16:42:42 +0530

    complete Phase 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const d = new Date();
+let year = d.getFullYear();
+
+function Footer(){
+    return <footer>
+        <p>Copyright @ {year}</p>
+        </footer>
+}
+
+export default Footer;
